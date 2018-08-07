4bd49e219214cd2190350bb73119416fe8d0724d
igorbuts <ibuts@cogniance.com>
2018-08-08 01:43:24 +0300

    Bootstrap Vue project with vue cli

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <h1>Gallery</h1>
+    <Gallery />
+  </div>
+</template>
+
+<script>
+import Gallery from './components/Gallery.vue';
+
+export default {
+  name: "app",
+  components: {
+    Gallery
+  }
+};
+</script>
+
+<style lang="scss">
+#app {
+  h1 {
+    text-transform: uppercase;
+    color: silver;
+    font-size: 4rem;
+    letter-spacing: 40px;
+  }
+
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ src/components/Gallery.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="Gallery">
+    TBD
+    {{ rowsCount }}
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Gallery',
+    props: {
+      // msg: String
+    },
+    data: () => {
+    const rowsCount = 3
+
+    return {
+      rowsCount
+    }
+  },
+  }
+</script>
+
+<style scoped lang="scss">
+
+</style>
+
